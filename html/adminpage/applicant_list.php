7c53a0edac6f50709c213b3cbaca5aa37c95bffb
CHESSDOK <mharkcastillocastalone@gmail.com>
2024-10-13 07:45:25 +0800

    finalizing5

--- html/adminpage/applicant_list.php
+++ html/adminpage/applicant_list.php
@@ -72,7 +72,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="../../css/modal-form.css">
     <link rel="stylesheet" href="../../css/nav_float.css">
     <link rel="stylesheet" href="../../css/admin_employer.css">
@@ -160,12 +160,12 @@
                 
                         if (!empty($applicants)) {
                             foreach ($applicants as $row) {
-                                $full_name = htmlspecialchars($row['first_name'] . ' ' . $row['middle_name'] . ' ' . $row['last_name']);
+                                $full_name = htmlspecialchars($row['first_name'] . ' ' . $row['middle_name'] . '. ' . $row['last_name']);
                                 $status = $row['status'];
                     
                                 echo "
                                 <tr>
-                                    <td style='width: 450px;'><?php echo htmlspecialchars($full_name); ?></td>
+                                    <td style='width: 450px;'>".htmlspecialchars($full_name)."</td>
 
                                     <td style='width: 100px;'>" . htmlspecialchars($row['job']) . "</td>
                                     <td style='width: 50px;'>" . ucfirst($status) . "</td>
@@ -183,7 +183,7 @@
                                     data-job-id=" . htmlspecialchars($row["job_posting_id"]) . ">Interview</button>";
                                 
                                 }elseif ($status === 'interview') {
-                                        echo " <a class='btn btn-success mx-2' href='application_process.php?id= ". htmlspecialchars($row['user_id']). "'>Accept</a>
+                                        echo " <a class='btn btn-success mx-2' href='   .php?id= ". htmlspecialchars($row['user_id']). "'>Accept</a>
                                                 <a class='btn btn-danger mx-2' href='application_rejection.php?id=" . htmlspecialchars($row['user_id']) ."&job_id=" . htmlspecialchars($row['job_posting_id']) ."'>Reject</a>";
                                 }
                     
@@ -354,8 +354,7 @@
     document.getElementById('date').setAttribute('min', currentDate);
     </script>
 
-    
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <script src="../../javascript/script.js"></script> 
 </body>
