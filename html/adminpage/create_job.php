effb0cf6f575219490736e827d8dc873f6122469
CHESDOK <mharkcastillocastalone@gmail.com>
2024-10-14 11:26:44 +0800

    update comment

--- html/adminpage/create_job.php
+++ html/adminpage/create_job.php
@@ -15,6 +15,7 @@ $result = $conn->query($sql);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     
     <link rel="stylesheet" href="../../css/modal-form.css"> 
     <link rel="stylesheet" href="../../css/nav_float.css">
@@ -150,10 +151,6 @@ $result = $conn->query($sql);
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="../../javascript/script.js"></script> <!-- You can link your JavaScript file here if needed -->
     <script>
             const jobModal = document.getElementById('jobModal');
             const seccloseModuleBtn = document.querySelector('.seccloseBtn');
@@ -185,5 +182,9 @@ $result = $conn->query($sql);
                 }
             });
     </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <script src="../../javascript/script.js"></script>
 </body>
 </html>
